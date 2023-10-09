c33c1eba37e48f96475f9e390152ab5b8ed87ed3
Derek Marion <derek.j.marion@gmail.com>
2023-10-09 10:47:57 -0700

    Python - write tests to work with pytest

--- python/credit_check_spec.py
+++ python/credit_check_spec.py
@@ -2,10 +2,12 @@
 
 from credit_check import credit_check
 
-print(credit_check('5541808923795240') == "The number is valid!")
-print(credit_check("4024007136512380") == "The number is valid!")
-print(credit_check("6011797668867828") == "The number is valid!")
+def test_valid():
+    assert credit_check('5541808923795240') == "The number is valid!"
+    assert credit_check("4024007136512380") == "The number is valid!"
+    assert credit_check("6011797668867828") == "The number is valid!"
 
-print(credit_check("5541801923795240") == "The number is invalid!")
-print(credit_check("4024007106512380") == "The number is invalid!")
-print(credit_check("6011797668868728") == "The number is invalid!")
+def test_invalid():
+    assert credit_check("5541801923795240") == "The number is invalid!"
+    assert credit_check("4024007106512380") == "The number is invalid!"
+    assert credit_check("6011797668868728") == "The number is invalid!"
